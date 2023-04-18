baea4ff16b20a67700c0d02a356bb9abdb8caf86
PHKhande <phk151raj@gmail.com>
2023-04-18 20:08:06 +0530

    Task01 Getting Started with Mongo DB

--- util/database.js
+++ util/database.js
@@ -1,8 +1,33 @@
-const Sequelize = require('sequelize');
+const mongodb = require('mongodb');
+const MongoClient = mongodb.MongoClient;
 
-const sequelize = new Sequelize('node-complete', 'root', 'nodecomplete', {
-  dialect: 'mysql',
-  host: 'localhost'
-});
+let _db;
 
-module.exports = sequelize;
+const mongoConnect = async(cb) => {
+
+  try{
+
+    const client = await MongoClient.connect('mongodb+srv://prathvirajkhande:prathviraj15@clustersharp.wegl4oc.mongodb.net/shop?retryWrites=true&w=majority');
+    console.log('Connected');
+    _db = client.db();
+    cb();
+
+  } catch(err) {
+    console.log(err)
+    throw err;
+  }
+
+};
+
+const getDb = () => {
+  
+  if (_db){
+    return _db;
+  }
+
+  throw 'No database found!';
+
+}
+
+exports.mongoConnect = mongoConnect;
+exports.getDb = getDb;
