6d400fe5f9984f4173da1eb6592d15152756d67f
PHKhande <phk151raj@gmail.com>
2023-04-19 21:15:27 +0530

    Task 02 Integrating CRUD with MongoDB

--- models/product.js
+++ models/product.js
@@ -1,11 +1,13 @@
+const mongodb = require('mongodb');
 const getDb = require('../util/database').getDb;
 
 class Product{
-  constructor(title, price, description, imageUrl){
+  constructor(title, price, description, imageUrl, id){
     this.title = title,
     this.price = price,
     this.description = description,
-    this.imageUrl = imageUrl
+    this.imageUrl = imageUrl,
+    this._id = id
   }
 
   async save(){
@@ -13,9 +15,57 @@ class Product{
     try{
 
       const db = getDb();
-      const result = await db.collection('products').insertOne(this);
-      console.log(result);
-      return result;
+      let dbOp;
+
+      if(this._id){
+        dbOp = await db.collection('products').updateOne({ _id: new mongodb.ObjectId(this._id) }, { $set: this });
+
+      } else {
+        dbOp = await db.collection('products').insertOne(this);
+
+      }
+      return dbOp;
+
+    } catch(err) {
+      console.log(err);
+
+    }
+    
+  }
+
+  static async fetchAll() {
+
+    try{
+      
+      const db = getDb();
+      return await db.collection('products').find().toArray(); 
+
+    } catch(err) {
+
+      console.log(err);
+    
+    }
+  }
+
+  static async findById(prodId){
+    
+    try{
+
+      const db = getDb();
+      return await db.collection('products').findOne({ _id: new mongodb.ObjectId(prodId)});
+
+    } catch(err) {
+
+      console.log(err);
+
+    }
+  }
+
+  static async deleteById(prodId){
+
+    try{
+      const db = getDb(); 
+      return await db.collection('products').deleteOne( { _id: new mongodb.ObjectId(prodId)} )
 
     } catch(err) {
 
